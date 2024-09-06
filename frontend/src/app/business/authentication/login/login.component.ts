87d42712f6360ec1a1a70ce6569bce11f7b89c69
JEIJEY <jeijey.avi@gmail.com>
2024-09-05 19:07:24 -0500

    terminando el login

--- frontend/src/app/business/authentication/login/login.component.ts
+++ frontend/src/app/business/authentication/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export default class LoginComponent {
+  @ViewChild('signInBtn') signInBtn!: ElementRef<HTMLButtonElement>;
+  @ViewChild('signUpBtn') signUpBtn!: ElementRef<HTMLButtonElement>;
+  @ViewChild('container') container!: ElementRef<HTMLDivElement>;
 
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    // Agrega la clase 'sign-up-mode' al contenedor cuando el botón "Sign up" es clicado
+    this.signUpBtn.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.add('sign-up-mode');
+    });
+
+    // Remueve la clase 'sign-up-mode' del contenedor cuando el botón "Sign in" es clicado
+    this.signInBtn.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.remove('sign-up-mode');
+    });
+  }
 }
