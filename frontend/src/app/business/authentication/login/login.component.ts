f133eeca8a047987d43abfcb25cc7cf21661cb18
JEIJEY <jeijey.avi@gmail.com>
2024-09-08 07:35:56 -0500

    responsive terminado login

--- frontend/src/app/business/authentication/login/login.component.ts
+++ frontend/src/app/business/authentication/login/login.component.ts
@@ -5,23 +5,37 @@ import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export default class LoginComponent {
+export default class LoginComponent implements AfterViewInit {
   @ViewChild('signInBtn') signInBtn!: ElementRef<HTMLButtonElement>;
   @ViewChild('signUpBtn') signUpBtn!: ElementRef<HTMLButtonElement>;
+  @ViewChild('signUpLink') signUpLink!: ElementRef<HTMLAnchorElement>;
+  @ViewChild('signInLink') signInLink!: ElementRef<HTMLAnchorElement>;
   @ViewChild('container') container!: ElementRef<HTMLDivElement>;
 
   constructor() { }
 
   ngAfterViewInit(): void {
-    // Agrega la clase 'sign-up-mode' al contenedor cuando el botón "Sign up" es clicado
+    // Maneja el clic en el botón de registro
     this.signUpBtn.nativeElement.addEventListener('click', () => {
       this.container.nativeElement.classList.add('sign-up-mode');
     });
 
-    // Remueve la clase 'sign-up-mode' del contenedor cuando el botón "Sign in" es clicado
+    // Maneja el clic en el botón de inicio de sesión
     this.signInBtn.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.remove('sign-up-mode');
+    });
+
+    // Maneja el clic en el enlace de registro (desde el formulario de inicio de sesión)
+    this.signUpLink.nativeElement.addEventListener('click', (event) => {
+      event.preventDefault(); // Previene la acción por defecto del enlace
+      this.container.nativeElement.classList.add('sign-up-mode');
+    });
+
+    // Maneja el clic en el enlace de inicio de sesión (desde el formulario de registro)
+    this.signInLink.nativeElement.addEventListener('click', (event) => {
+      event.preventDefault(); // Previene la acción por defecto del enlace
       this.container.nativeElement.classList.remove('sign-up-mode');
     });
   }
